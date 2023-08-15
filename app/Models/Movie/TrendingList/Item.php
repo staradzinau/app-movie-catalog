50fac37114f1c93d623d682b5aa10372acb33d39
Anton Staradzinau <staradzinau@gmail.com>
2023-08-16 01:47:47 +0200

    feat: Trending movie list backend
    Pt. 1. Add model + migration

---
+++ app/Models/Movie/TrendingList/Item.php
@@ -0,0 +1,179 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Models\Movie\TrendingList;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+class Item extends Model
+{
+    /**
+     * Name of the DB table, used by the model
+     */
+    public const MAIN_TABLE_NAME = 'movies_trending_list';
+
+    /**
+     * Constants for the model fields
+     */
+    public const ID = 'id';
+    public const MOVIE_ID = 'movie_id';
+    public const IS_TRENDING_DAILY = 'is_trending_daily';
+    public const IS_TRENDING_WEEKLY = 'is_trending_weekly';
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = self::MAIN_TABLE_NAME;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        self::MOVIE_ID,
+        self::IS_TRENDING_DAILY,
+        self::IS_TRENDING_WEEKLY,
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        self::CREATED_AT => 'datetime',
+        self::UPDATED_AT => 'datetime',
+        self::IS_TRENDING_DAILY => 'bool',
+        self::IS_TRENDING_WEEKLY => 'bool',
+    ];
+
+    /**
+     * Retrieve item id
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->getAttribute(self::ID);
+    }
+
+    /**
+     * Set item id
+     *
+     * @param int $id
+     * @return $this
+     */
+    public function setId(int $id): Item
+    {
+        return $this->setAttribute(self::ID, $id);
+    }
+
+    /**
+     * Retrieve movie id
+     *
+     * @return int
+     */
+    public function getMovieId(): int
+    {
+        return $this->getAttribute(self::MOVIE_ID);
+    }
+
+    /**
+     * Set movie id
+     *
+     * @param int $movieId
+     * @return $this
+     */
+    public function setMovieId(int $movieId): Item
+    {
+        return $this->setAttribute(self::MOVIE_ID, $movieId);
+    }
+
+    /**
+     * Retrieve is trending daily flag
+     *
+     * @return bool
+     */
+    public function getIsTrendingDaily(): bool
+    {
+        return $this->getAttribute(self::IS_TRENDING_DAILY);
+    }
+
+    /**
+     * Set is trending daily flag
+     *
+     * @param bool $isTrendingDaily
+     * @return $this
+     */
+    public function setIsTrendingDaily(bool $isTrendingDaily): Item
+    {
+        return $this->setAttribute(self::IS_TRENDING_DAILY, $isTrendingDaily);
+    }
+
+    /**
+     * Retrieve is trending weekly flag
+     *
+     * @return bool
+     */
+    public function getIsTrendingWeekly(): bool
+    {
+        return $this->getAttribute(self::IS_TRENDING_WEEKLY);
+    }
+
+    /**
+     * Set is trending weekly flag
+     *
+     * @param bool $isTrendingWeekly
+     * @return $this
+     */
+    public function setIsTrendingWeekly(bool $isTrendingWeekly): Item
+    {
+        return $this->setAttribute(self::IS_TRENDING_WEEKLY, $isTrendingWeekly);
+    }
+
+    /**
+     * Retrieve datetime of list item creation
+     *
+     * @return Carbon|null
+     */
+    public function getDatetimeOfCreation(): ?Carbon
+    {
+        return $this->getAttribute(self::CREATED_AT);
+    }
+
+    /**
+     * Set datetime of list item creation
+     *
+     * @param Carbon|null $datetimeOfCreation
+     * @return $this
+     */
+    public function setDatetimeOfCreation(?Carbon $datetimeOfCreation): Item
+    {
+        return $this->setAttribute(self::CREATED_AT, $datetimeOfCreation);
+    }
+
+    /**
+     * Retrieve datetime of list item last update
+     *
+     * @return Carbon|null
+     */
+    public function getDatetimeOfLastUpdate(): ?Carbon
+    {
+        return $this->getAttribute(self::UPDATED_AT);
+    }
+
+    /**
+     * Set datetime of list item last update
+     *
+     * @param Carbon|null $datetimeOfLastUpdate
+     * @return $this
+     */
+    public function setDatetimeOfLastUpdate(?Carbon $datetimeOfLastUpdate): Item
+    {
+        return $this->setAttribute(self::UPDATED_AT, $datetimeOfLastUpdate);
+    }
+}
---
+++ database/migrations/2023_08_15_234410_create_movies_trending_list_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Movie\TrendingList\Item as MovieTrendingListItem;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(
+            MovieTrendingListItem::MAIN_TABLE_NAME,
+            function (Blueprint $table) {
+                $table->id(MovieTrendingListItem::ID);
+                $table->timestamps();
+                $table->unsignedInteger(MovieTrendingListItem::MOVIE_ID);
+                $table->boolean(MovieTrendingListItem::IS_TRENDING_DAILY);
+                $table->boolean(MovieTrendingListItem::IS_TRENDING_WEEKLY);
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(MovieTrendingListItem::MAIN_TABLE_NAME);
+    }
+};
