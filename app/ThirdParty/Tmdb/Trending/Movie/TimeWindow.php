00b1d6fb2b9d4a94ed12f98797ef5a242b05b180
Anton Staradzinau <staradzinau@gmail.com>
2023-08-25 00:05:40 +0200

    feat: Movie trending list frontend
    Pt. 4. Grid: time window support
    Phase 3. Add human-readable values for enum

--- app/ThirdParty/Tmdb/Trending/Movie/TimeWindow.php
+++ app/ThirdParty/Tmdb/Trending/Movie/TimeWindow.php
@@ -7,4 +7,17 @@ enum TimeWindow: string
 {
     case DAY = 'day';
     case WEEK = 'week';
+
+    /**
+     * Retrieve human-readable representation of the enum current value
+     *
+     * @return string
+     */
+    public function label(): string
+    {
+        return match($this) {
+            self::DAY => 'Today',
+            self::WEEK => 'This Week',
+        };
+    }
 }
